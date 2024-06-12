6bac53abe01e4cd066fb0878ec1c7a880aee6b06
shahidmukadam <shahid.mukadam@cafu.com>
2024-06-12 18:12:55 +0530

    Second commit - 1. Create make booking function

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import 
-("fmt"
-"strings"
+import (
+	"fmt"
+	"strings"
 )
+
 var conferenceName = "Go conference"
+
 const conferenceTickets = 50
+
 var remainingTickets uint = 50
 
 func main() {
@@ -14,33 +17,39 @@ func main() {
 	var userLastName string
 	var userEmail string
 	var usertickets uint
- 
+	for {
+		if remainingTickets > 0 {
+			greetUser()
 
-    greetUser()
+			userFirstName, userLastName, userEmail, usertickets = getuserdata()
+			var isuserdatavalid, issue = validateUserData(userFirstName, userLastName, userEmail)
 
-	userFirstName , userLastName ,userEmail, usertickets = getuserdata()
+			//fmt.Printf("The user data provided is %v and issue observed is in %v \n", isuserdatavalid , issue)
+			if !isuserdatavalid {
+				fmt.Printf("Please submit valid data, issue is in: %v \n", issue)
+				fmt.Print("***************************************************")
 
+			} else if usertickets > remainingTickets {
+				fmt.Printf("Tickets unavailable for the required amount \n")
+			} else {
+				makeBooking(userFirstName, userLastName, userEmail, usertickets)
+			}
 
+		} else {
+			break
+		}
 
-	var isuserdatavalid, issue =  validateUserData(userFirstName , userLastName , userEmail )
-
-	//fmt.Printf("The user data provided is %v and issue observed is in %v \n", isuserdatavalid , issue)
-	if !isuserdatavalid  {
-		fmt.Printf("Please submit valid data, issue is in %v \n",issue)
-
-	}else if usertickets > remainingTickets {
-		fmt.Printf("Tickets unavailable for the required amount \n")
-	}else {
-		remainingTickets-= usertickets
-		fmt.Printf("Thanks %v %v for booking %v ticket(s). The ticket will be emailed on %v \n", userFirstName,userLastName , usertickets,userEmail)
-		fmt.Printf("%v tickets remaining\n",remainingTickets)
 	}
-
-
 
 }
 
-func getuserdata()(string,string,string,uint){
+func makeBooking(userFirstName string, userLastName string, userEmail string, usertickets uint) {
+	remainingTickets -= usertickets
+	fmt.Printf("Thanks %v %v for booking %v ticket(s). The ticket will be emailed on %v \n", userFirstName, userLastName, usertickets, userEmail)
+	fmt.Printf("%v tickets remaining\n", remainingTickets)
+	fmt.Print("***************************************************")
+}
+func getuserdata() (string, string, string, uint) {
 
 	var userFirstName string
 	var userLastName string
@@ -52,45 +61,45 @@ func getuserdata()(string,string,string,uint){
 
 	fmt.Println("Enter user Last Name")
 	fmt.Scan(&userLastName)
-	
+
 	fmt.Println("Enter user email")
 	fmt.Scan(&userEmail)
-	
-	fmt.Println("Enter how many tickets you want to book?")
-	fmt.Scan(&usertickets)	
 
-	return userFirstName,userLastName,userEmail,usertickets
+	fmt.Println("Enter how many tickets you want to book?")
+	fmt.Scan(&usertickets)
+
+	return userFirstName, userLastName, userEmail, usertickets
 
 }
- func validateUserData(userFirstName string, userLastName string , userEmail string) (bool,string){
+func validateUserData(userFirstName string, userLastName string, userEmail string) (bool, string) {
 	var isvalid = false
 	var issue = ""
-	if strings.Count(userFirstName,"")>= 3 {
+	if strings.Count(userFirstName, "") >= 3 {
 
-		if strings.Count(userLastName,"") >= 3{
-			if strings.Contains(userEmail,"@"){
+		if strings.Count(userLastName, "") >= 3 {
+			if strings.Contains(userEmail, "@") {
 				isvalid = true
 			} else {
-				issue= "userEmail"
-				return isvalid , issue
+				issue = "userEmail"
+				return isvalid, issue
 
 			}
-				
-		}else {
+
+		} else {
 			issue = "userLastName"
-			return isvalid , issue
+			return isvalid, issue
 		}
-	}else {
+	} else {
 		issue = "userFirstName"
-		return isvalid , issue
-	 }
-	 
-	 return isvalid , issue
- }
+		return isvalid, issue
+	}
 
- func greetUser(){
+	return isvalid, issue
+}
 
-	fmt.Printf("Welcome to %v booking application \n",conferenceName)
-	fmt.Printf("We have a total of %v and %v tickets are still available \n",conferenceTickets,remainingTickets)
+func greetUser() {
+
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Printf("We have a total of %v and %v tickets are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Book your tickets with us now")
- }
+}
