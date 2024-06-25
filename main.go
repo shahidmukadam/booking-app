aa09c59150b72f6a2f68987977ef74e519bd395f
shahidmukadam <shahid.mukadam@cafu.com>
2024-06-25 11:24:49 +0530

    making methods to the struct instead of functions

--- main.go
+++ main.go
@@ -4,31 +4,46 @@ import (
 	"booking-app/sharedlib"
 	"fmt"
 	"strings"
+	//"slices"
 )
 
-
 var ConfData = make([]ConferenceEvent, 0)
-var BookingData =make([]ConfUser ,0)
+var BookingData = make([]ConfUser, 0)
 
 type ConferenceEvent struct {
-	conferenceName string
-	conferenceLocation string
-	conferenceTickets uint
+	conferenceName       string
+	conferenceLocation   string
+	conferenceTickets    uint
 	remainingconfTickets uint
-	Confeventuser []ConfUser
-
+	Confeventuser        []ConfUser
 }
-type ConfUser struct{
-	firstName string
-	lastName string
-	userEmail string
+//Methods of the Conference event
+func (CE ConferenceEvent) makebooking(userFirstName string, userLastName string, userEmail string, usertickets uint, remainingconftickets uint) ConfUser {
+	remainingconftickets -= usertickets
+	//storing booking data to a customer profile
+	var userData = ConfUser{
+		firstName:   userFirstName,
+		lastName:    userLastName,
+		userEmail:   userEmail,
+		usertickets: usertickets,
+	}
+	fmt.Printf("Thanks %v %v for booking %v ticket(s). The ticket will be emailed on %v \n", userFirstName, userLastName, usertickets, userEmail)
+	fmt.Printf("%v tickets remaining\n", remainingconftickets)
+	fmt.Println("***************************************************")
+	return userData
+}
+
+type ConfUser struct {
+	firstName   string
+	lastName    string
+	userEmail   string
 	usertickets uint
 }
 
-func main(){
+func main() {
 	var userChoice string
 	for {
-		// greet the user and ask what the user wants to do 
+		// greet the user and ask what the user wants to do
 		fmt.Println("Welcome to the conference app ")
 		fmt.Println("What do you want to do ?")
 		fmt.Println("Press 1 to create a new conference ")
@@ -44,8 +59,8 @@ func main(){
 			fmt.Println("1 pressed")
 			fmt.Println("We will create a new conference for you, Enter the conference Name")
 			fmt.Scan(&confname)
-			
-			if ValidateConfName(confname){
+
+			if ValidateConfName(confname) {
 				CreateConference(confname)
 			} else {
 				fmt.Println("Conference with the same name exists, please create a conference with a different name")
@@ -59,28 +74,27 @@ func main(){
 			fmt.Println("Enter the name of the conference you want to book ticket for")
 			fmt.Scan(&confnametobook)
 
-			for k ,conf := range ConfData{
-				if confnametobook==conf.conferenceName && conf.remainingconfTickets > 0 {
-						sharedlib.GreetUser(conf.conferenceName,conf.conferenceTickets,conf.remainingconfTickets)
-						userFirstName , userLastName , userEmail , userTicket := getuserdata()
-						var isuserdatavalid, issue = validateUserData(userFirstName, userLastName, userEmail)
-						if !isuserdatavalid {
-							fmt.Printf("Please submit valid data, issue is in: %v \n", issue)
-							fmt.Print("***************************************************")
-			
-						} else if userTicket > conf.remainingconfTickets {
-							fmt.Printf("Tickets unavailable for the required amount \n")
-						} else {
-							var pointertoconfuser = ConfUser{
-							}
-							conf.remainingconfTickets,pointertoconfuser = makeBooking(userFirstName, userLastName, userEmail, userTicket,conf.remainingconfTickets)
-							conf.Confeventuser = append(conf.Confeventuser, pointertoconfuser) 
-							ConfData[k] =conf
-							fmt.Printf("The name of conference booked is %v \n",conf.conferenceName)
-							fmt.Printf("Number of tickets remaining are %v \n",conf.remainingconfTickets)
-							fmt.Printf("Booking details till now are %v \n",conf.Confeventuser)
-							fmt.Printf("confData: %v\n", ConfData)
-						}
+			for k, conf := range ConfData {
+				if confnametobook == conf.conferenceName && conf.remainingconfTickets > 0 {
+					sharedlib.GreetUser(conf.conferenceName, conf.conferenceTickets, conf.remainingconfTickets)
+					userFirstName, userLastName, userEmail, userTicket := getuserdata()
+					var isuserdatavalid, issue = validateUserData(userFirstName, userLastName, userEmail)
+					if !isuserdatavalid {
+						fmt.Printf("Please submit valid data, issue is in: %v \n", issue)
+						fmt.Print("***************************************************")
+
+					} else if userTicket > conf.remainingconfTickets {
+						fmt.Printf("Tickets unavailable for the required amount \n")
+					} else {
+						var pointertoconfuser = ConfUser{}
+						pointertoconfuser = conf.makebooking(userFirstName, userLastName, userEmail, userTicket, conf.remainingconfTickets)
+						conf.Confeventuser = append(conf.Confeventuser, pointertoconfuser)
+						ConfData[k] = conf
+						fmt.Printf("The name of conference booked is %v \n", conf.conferenceName)
+						fmt.Printf("Number of tickets remaining are %v \n", conf.remainingconfTickets)
+						fmt.Printf("Booking details till now are %v \n", conf.Confeventuser)
+						fmt.Printf("confData: %v\n", ConfData)
+					}
 				}
 			}
 
@@ -91,30 +105,30 @@ func main(){
 			fmt.Println("Enter the name of the conference you want to view tickets for")
 			fmt.Scan(&confnametoview)
 
-			for _,conf := range ConfData{
+			for _, conf := range ConfData {
 				if confnametoview == conf.conferenceName {
-					for _,user := range conf.Confeventuser{
-						fmt.Printf("The user %v has booked %v tickets \n",user.firstName +""+user.lastName , user.usertickets)
+					for _, user := range conf.Confeventuser {
+						fmt.Printf("The user %v has booked %v tickets \n", user.firstName+""+user.lastName, user.usertickets)
 					}
 				}
 			}
-			case "4": printConfNames()
+		case "4":
+			printConfNames()
 
-
-			default : 
+		default:
 			fmt.Println("Press a valid option")
-				
-			}
 
-			
 		}
+
 	}
+}
+
 /*
 func makeconferencebooking(conf ConferenceEvent, k int){
 	var pointertoconfuser = ConfUser{
 	}
 	conf.remainingconfTickets,pointertoconfuser = makeBooking(userFirstName, userLastName, userEmail, userTicket,conf.remainingconfTickets)
-	conf.Confeventuser = append(conf.Confeventuser, pointertoconfuser) 
+	conf.Confeventuser = append(conf.Confeventuser, pointertoconfuser)
 	ConfData[k] =conf
 	fmt.Printf("The name of conference booked is %v \n",conf.conferenceName)
 	fmt.Printf("Number of tickets remaining are %v \n",conf.remainingconfTickets)
@@ -123,53 +137,52 @@ func makeconferencebooking(conf ConferenceEvent, k int){
 }
 */
 
-func printConfNames(){
-	for i ,conf := range ConfData{
-		fmt.Printf("Name of the %v conference is %v\n", i+1,conf.conferenceName)
+func printConfNames() {
+	for i, conf := range ConfData {
+		fmt.Printf("Name of the %v conference is %v\n", i+1, conf.conferenceName)
 	}
 }
 func ValidateConfName(confName string) bool {
 
 	var isconfnameValid = true
-			for _,conf := range ConfData {
-				if confName == conf.conferenceName {
-					isconfnameValid = false
-					return isconfnameValid
-				}
+	for _, conf := range ConfData {
+		if confName == conf.conferenceName {
+			isconfnameValid = false
+			return isconfnameValid
+		}
 
-}
-return isconfnameValid
+	}
+	return isconfnameValid
 }
 
-func CreateConference(confname string){
-	var confdat = ConferenceEvent {
+func CreateConference(confname string) {
+	var confdat = ConferenceEvent{
 		conferenceName: confname,
-
 	}
 	fmt.Println("Enter the location of the conference")
 	fmt.Scan(&confdat.conferenceLocation)
 	fmt.Println("Enter the number of ticekts in the conference")
 	fmt.Scan(&confdat.conferenceTickets)
-	confdat.remainingconfTickets=confdat.conferenceTickets
+	confdat.remainingconfTickets = confdat.conferenceTickets
 	ConfData = append(ConfData, confdat)
 }
 
+func makeBooking(userFirstName string, userLastName string, userEmail string, usertickets uint, remainingTickets uint) (uint, ConfUser) {
 
-func makeBooking(userFirstName string, userLastName string, userEmail string, usertickets uint,remainingTickets uint) (uint ,ConfUser) {
-	
 	remainingTickets -= usertickets
 	//storing booking data to a customer profile
 	var userData = ConfUser{
-		firstName: userFirstName,
-		lastName: userLastName,
-		userEmail: userEmail,
+		firstName:   userFirstName,
+		lastName:    userLastName,
+		userEmail:   userEmail,
 		usertickets: usertickets,
 	}
 	fmt.Printf("Thanks %v %v for booking %v ticket(s). The ticket will be emailed on %v \n", userFirstName, userLastName, usertickets, userEmail)
 	fmt.Printf("%v tickets remaining\n", remainingTickets)
 	fmt.Println("***************************************************")
-	return remainingTickets,userData
+	return remainingTickets, userData
 }
+
 func getuserdata() (string, string, string, uint) {
 
 	var userFirstName string
@@ -192,7 +205,6 @@ func getuserdata() (string, string, string, uint) {
 	return userFirstName, userLastName, userEmail, usertickets
 
 }
-
 
 func validateUserData(userFirstName string, userLastName string, userEmail string) (bool, string) {
 	var isvalid = false
